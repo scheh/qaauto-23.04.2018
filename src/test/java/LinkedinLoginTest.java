63f32e8bb74cd19cebb018c92e7a0d3dbe035620
scheh <sths@ukr.net>
2018-05-14 18:58:10 +0300

    HomeWork4

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
+import static org.testng.Assert.assertEquals;
 
 public class LinkedinLoginTest
 {
@@ -21,7 +22,7 @@ public class LinkedinLoginTest
     String actualLoginPageTitle = webDriver.getTitle();
 
     //Assert for title
-        Assert.assertEquals(webDriver.getTitle(),
+        assertEquals(webDriver.getTitle(),
                 "LinkedIn: Log In or Sign Up",
                 "Login page Title is wrong");
 
@@ -31,8 +32,8 @@ public class LinkedinLoginTest
                 //"https://www.linkedin.com/",
                 //"Wrong URL");
 
-        Assert.assertEquals(webDriver.getCurrentUrl(),
-            "https://linkedin.com/",
+        assertEquals(webDriver.getCurrentUrl(),
+            "https://www.linkedin.com/",
             "Incorrect URL");
 
     //Assert for login field presence
@@ -49,6 +50,114 @@ public class LinkedinLoginTest
         WebElement okButton = webDriver.findElement(By.xpath("//input[@class='login submit-button']"));
         Assert.assertTrue(okButton.isDisplayed(),
                 "OK button is absent");
+
+    //Negative cases
+
+    //Case1
+        loginField.sendKeys("scheh@adyax.com");
+        passwordField.clear();
+        okButton.click();
+
+        Assert.assertTrue(loginField.isDisplayed());
+        Assert.assertTrue(passwordField.isDisplayed());
+        Assert.assertTrue(okButton.isDisplayed());
+
+    //Case2
+        loginField.clear();
+        passwordField.sendKeys("Password");
+        okButton.click();
+
+        Assert.assertTrue(loginField.isDisplayed());
+        Assert.assertTrue(passwordField.isDisplayed());
+        Assert.assertTrue(okButton.isDisplayed());
+
+    //Case3
+        loginField.sendKeys("schehschehscheh");
+        passwordField.sendKeys("password");
+        okButton.click();
+
+        sleep(3000);
+
+        WebElement errorMessage1 = webDriver.findElement(By.xpath("//strong"));
+        Assert.assertTrue(errorMessage1.isDisplayed(),
+                "Message is absent");
+
+        WebElement errorMessage2 = webDriver.findElement(By.xpath("//*[@id='session_key-login-error']"));
+        Assert.assertTrue(errorMessage2.isDisplayed(),
+                "Message is absent");
+
+        WebElement errorText1 = webDriver.findElement(By.xpath("//*[@id='control_gen_1']/p/strong"));
+        Assert.assertEquals(errorText1.getText(),
+                "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.",
+                "Error message is wrong");
+
+        WebElement errorText2 = webDriver.findElement(By.xpath("//*[@id='session_key-login-error']"));
+        Assert.assertEquals(errorText2.getText(),
+                "Укажите действительный адрес эл. почты.",
+                "Error message is wrong");
+
+        sleep(3000);
+
+    //Case4
+        WebElement emailField = webDriver.findElement(By.xpath("//*[@id='session_key-login']"));
+        WebElement passField = webDriver.findElement(By.xpath("//*[@id='session_password-login']"));
+        WebElement signInButton = webDriver.findElement(By.id("btn-primary"));
+
+        emailField.clear();
+        passField.clear();
+
+        emailField.sendKeys("scheh@adyax.com");
+        passField.sendKeys("123456");
+        signInButton.click();
+
+        sleep(5000);
+
+
+        Assert.assertTrue(errorMessage1.isDisplayed(),
+                "Error message is absent");
+
+        Assert.assertEquals(errorText1.getText(),
+                "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.",
+                "Error message is wrong");
+
+        Assert.assertEquals(errorText2.getText(),
+                "Укажите действительный адрес эл. почты.",
+                "Error message is wrong");
+
+    //Case5
+        emailField.sendKeys("scheeeeehh@ady.ua");
+        passField.sendKeys("Password0123");
+        signInButton.click();
+        sleep(3000);
+
+        Assert.assertTrue(errorMessage1.isDisplayed(),
+                "Error message is absent");
+
+        Assert.assertTrue(errorMessage2.isDisplayed(),
+                "Error message is absent");
+
+        Assert.assertEquals(errorText1.getText(),
+                "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.",
+                "Error message is wrong");
+
+        Assert.assertEquals(errorText2.getText(),
+                "Укажите действительный адрес эл. почты.",
+                "Error message is wrong");
+
+    //Case6
+        emailField.clear();
+        passField.clear();
+        signInButton.click();
+
+        Assert.assertTrue(errorMessage1.isDisplayed());
+
+        Assert.assertEquals(errorText1.getText(),
+                "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.",
+                "Error message is wrong");
+
+        Assert.assertEquals(errorText2.getText(),
+                "Укажите действительный адрес эл. почты.",
+                "Error message is wrong");
 
     //Log in process
         loginField.sendKeys("sths@ukr.net");
@@ -75,7 +184,7 @@ public class LinkedinLoginTest
 
     //Assert for correct URL
         String loggedURL = webDriver.getCurrentUrl();
-        Assert.assertEquals(loggedURL,
+        assertEquals(loggedURL,
                 "https://www.linkedin.com/feed/",
                 "URL Incorrect" );
 
